f2e546cf7e17873ee055035daf9b8292de1bd7b6
Kalvin <kalvinmachful@hotmail.com>
2024-08-09 16:05:59 +0200

    contact form

--- Front-End-Fisheye-main/scripts/utils/contactForm.js
+++ Front-End-Fisheye-main/scripts/utils/contactForm.js
@@ -7,3 +7,23 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+// Gestionnaire de soumission du formulaire
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire
+
+    // Récupère les valeurs des champs du formulaire
+    const firstName = document.getElementById("first_name").value;
+    const lastName = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Affiche les valeurs dans la console
+    console.log("Prénom:", firstName);
+    console.log("Nom:", lastName);
+    console.log("Email:", email);
+    console.log("Message:", message);
+    // Réinitialise le formulaire
+    event.target.reset();
+    // Ferme la modale
+    closeModal();
+  });
